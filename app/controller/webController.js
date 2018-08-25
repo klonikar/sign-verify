1366b725a1820965a1c8ce8cf1529b16a18b18eb
Kiran Lonikar <klonikar@yahoo-inc.com>
2018-08-25 12:52:37 +0530

    minor change to make imported package as const

--- app/controller/webController.js
+++ app/controller/webController.js
@@ -1,9 +1,8 @@
 const sshpk = require('sshpk');
 const fs = require('fs');
-var crypto = require('crypto');
+const crypto = require('crypto');
 
 let webCtrl = function () {
-
     /**
      * Signed the message
      * @returns signature
@@ -13,7 +12,6 @@ let webCtrl = function () {
         /* Read in an OpenSSH/PEM *private* key */
         let keyPriv = fs.readFileSync(process.env.LICENSE_PRIVATE_KEY || (process.env.HOME + '/.ssh/id_rsa'));
         let key = sshpk.parsePrivateKey(keyPriv, 'pem');
-
 
         /* Sign some data with the key */
         let s = key.createSign('sha1');
@@ -26,7 +24,6 @@ let webCtrl = function () {
             "prodId": msg
         });
         res.end();
-
     };
 
     /**
@@ -35,11 +32,9 @@ let webCtrl = function () {
      * @returns true/false
      */
     this.verify = function (req, res) {
-
         /* Now load the public key*/
         let keyPub = fs.readFileSync(process.env.LICENSE_PUBLIC_KEY || (process.env.HOME + '/.ssh/id_rsa.pub'));
         let key = sshpk.parseKey(keyPub, 'ssh');
-
 
         /* Make a crypto.Verifier with this key */
         let v = key.createVerify('sha1');
